96647996205e8c5cbcf1cf61e91625cade7c799f
Michal Charemza <michal@charemza.name>
2021-05-16 21:11:20 +0100

    build(release): v0.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='stream-unzip',
+    version='0.0.1',
+    author='Department for International Trade',
+    author_email='sre@digital.trade.gov.uk',
+    description='Python function to stream unzip all the files in a ZIP archive, without loading the entire ZIP file into memory or any of its uncompressed files',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uktrade/stream-unzip',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6.0',
+    py_modules=[
+        'stream_unzip',
+    ],
+)
