f5801a687d6acbd15d3f4a4589477b3be4716dfe
Michal Charemza <michal@charemza.name>
2021-09-14 08:16:29 +0100

    refactor: remove unnecessary test

--- stream_unzip.py
+++ stream_unzip.py
@@ -272,7 +272,7 @@ def stream_unzip(zipfile_chunks, password=None, chunk_size=65536):
             Struct('<QQ').unpack(extra[zip64_size_signature]) if is_zip64 else \
             (uncompressed_size, compressed_size)
         uncompressed_size = \
-            None if has_data_descriptor and compression in (8, 99) else \
+            None if has_data_descriptor and compression == 8 else \
             uncompressed_size
 
         decompressor = \
